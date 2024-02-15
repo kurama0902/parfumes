338e26ed7b8339aaadb0951b01d2efeb69d01713
kurama0902 <creepysimbaplay@gmail.com>
2024-02-15 20:31:02 +0200

    created header UI & slider

---
+++ shop/src/app/_components/header/page.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import Image from 'next/image'
+
+import s from './header.module.css';
+
+export default function Header() {
+    return (
+        <header className={s['header-wrap']}>
+            <Link className={s['logo-link']} href="/">
+                <Image src="/logo.svg" width={160} height={43} unoptimized alt='Copper Pro' />
+            </Link>
+            <nav className={s['page-links-wrap']}>
+                <ul className={s['public-links']}>
+                    <Link className='catalog-link' href="/catalog">Каталог</Link>
+                    <Link className='news-link' href="/news">Новини</Link>
+                    <Link className='delivery-link' href="/delivery">Доставка</Link>
+                    <Link className='about-us-link' href="/about-us">Про нас</Link>
+                    <Link className='contacts-link' href="/contacts">Контакти</Link>
+                </ul>
+                <ul className={s['users-links']}>
+                    <button className={s['liked-popup-btn']}>
+                        <Image src='/heart.svg' width={30} height={26} unoptimized alt='Liked button' />
+                    </button>
+                    <button className={s['user-page-btn']}>
+                        <Image src='/user.svg' width={30} height={26} unoptimized alt='User page button' />
+                    </button>
+                    <Link className={s['cart-link']} href='/cart'>
+                        <Image src='/cart.svg' width={30} height={30} unoptimized alt='Cart link' />
+                    </Link>
+                </ul>
+            </nav>
+        </header>
+    )
+}
---
+++ shop/src/app/_components/slider/index.tsx
@@ -0,0 +1,93 @@
+import React, { ReactNode, useState } from "react"
+import "./style.css"
+import { useKeenSlider } from "keen-slider/react"
+import Image from 'next/image'
+import "keen-slider/keen-slider.min.css"
+
+export default function Slider() {
+  const [currentSlide, setCurrentSlide] = React.useState(0)
+  const [loaded, setLoaded] = useState(false)
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+    initial: 0,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel)
+    },
+    created() {
+      setLoaded(true)
+    },
+  })
+
+  return (
+    <>
+      <div className="navigation-wrapper">
+        <div ref={sliderRef} className="keen-slider">
+          <div className="keen-slider__slide number-slide1">1</div>
+          <div className="keen-slider__slide number-slide2">2</div>
+          <div className="keen-slider__slide number-slide3">3</div>
+          {loaded && instanceRef.current && (
+            <div className="dots">
+              {[
+                ...Array(instanceRef.current.track.details.slides.length).keys(),
+              ].map((idx) => {
+                return (
+                  <button
+                    key={idx}
+                    onClick={() => {
+                      instanceRef.current?.moveToIdx(idx)
+                    }}
+                    className={"dot" + (currentSlide === idx ? " active" : "")}
+                  ></button>
+                )
+              })}
+            </div>
+          )}
+        </div>
+        {loaded && instanceRef.current && (
+          <>
+            <Arrow
+              left
+              onClick={(e: any) =>
+                e.stopPropagation() || instanceRef.current?.prev()
+              }
+              disabled={currentSlide === 0}
+            />
+
+            <Arrow
+              onClick={(e: any) =>
+                e.stopPropagation() || instanceRef.current?.next()
+              }
+              disabled={
+                currentSlide ===
+                instanceRef.current.track.details.slides.length - 1
+              }
+            />
+          </>
+        )}
+      </div>
+    </>
+  )
+}
+
+function Arrow(props: {
+  disabled: boolean
+  left?: boolean
+  onClick: (e: any) => void
+}) {
+  const disabled = props.disabled ? " arrow--disabled" : ""
+  return (
+    <div onClick={props.onClick} className={`arrow-wrap ${props.left ? "arrow--left" : "arrow--right"
+  } ${disabled}`}>
+      <svg width="17.846283" height="28.000000" viewBox="0 0 17.8463 28" fill="none" xmlns="http://www.w3.org/2000/svg"
+
+    >
+      {props.left && (
+        <path id="Vector" d="M17.8463 24.1537L7.69257 14L17.8463 3.84631L14 0L0 14L14 28L17.8463 24.1537Z" fill="#FFFFFF" fillOpacity="1.000000" fillRule="nonzero" />
+
+      )}
+      {!props.left && (
+        <path id="Vector" d="M0 3.84631L10.1537 14L0 24.1537L3.84631 28L17.8463 14L3.84631 0L0 3.84631Z" fill="#FFFFFF" fillOpacity="1.000000" fillRule="nonzero" />
+      )}
+    </svg>
+    </div>
+  )
+}
