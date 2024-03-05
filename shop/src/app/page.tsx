af8730f8af592bbc8f9b1b95af1cc04f85b59ea6
kurama0902 <creepysimbaplay@gmail.com>
2024-03-05 12:37:56 +0200

    working on slider

--- shop/src/app/page.tsx
+++ shop/src/app/page.tsx
@@ -2,12 +2,23 @@
 
 import { Slider } from "~/app/components/slider";
 import { TypeOfProducts } from "./components/typeOfProducts";
+import { EmblaOptionsType } from 'embla-carousel'
+import { EmblaCarousel } from "./components/categorySlider";
+
+import './base.css'
+import './embla.css'
 
 export default function Home() {
+
+  const OPTIONS: EmblaOptionsType = { loop: true }
+  const SLIDE_COUNT = 3
+  const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+
   return (
     <div className="home-page-wrap">
       <Slider />
-      <TypeOfProducts/>
+      <TypeOfProducts />
+      <EmblaCarousel slides={SLIDES} options={OPTIONS} />
     </div>
   );
 }
